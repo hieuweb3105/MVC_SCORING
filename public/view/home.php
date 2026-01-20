b210c4b176f1b1e3a0468ea203ba0949ad081835
Minh Hiếu <nguyenhieu3105@gmail.com>
2026-01-20 14:58:38 +0700

    update

--- public/view/home.php
+++ public/view/home.php
@@ -2,7 +2,7 @@
 
 <div class="d-flex align-items-center justify-content-center px-3">
     <div class="d-flex flex-column gap-3 col-12 col-md-8 col-lg-6">
-        <h4 class="text-light text-center mb-5">Chọn tiết mục bạn muốn chấm điểm</h4>
+        <h4 class="text-light text-center mb-5">Chọn tiết mục để chấm điểm</h4>
         <?php foreach ($list_show as $show): extract($show) ?>
             <a href="/event/<?= $id ?>" class="btn btn-outline-light text-wrap py-3"><?= $name ?></a>
         <?php endforeach ?>
