ec6006409471b517d0e56e1c5270ed05c371124a
Minh Hiếu <nguyenhieu3105@gmail.com>
2026-01-23 22:38:50 +0700

    update

--- public/view/home.php
+++ public/view/home.php
@@ -2,7 +2,7 @@
 
 <div class="d-flex align-items-center justify-content-center px-3">
     <div class="d-flex flex-column gap-3 col-12 col-md-8 col-lg-6">
-        <h4 class="text-light text-center mb-5">Chọn tiết mục để chấm điểm</h4>
+        <h4 class="text-light text-center mb-5">ON Mixtape Showcase: Real-time Scoring</h4>
         <?php foreach ($list_show as $show): 
             $case_redirect = 'event';
             extract($show);
