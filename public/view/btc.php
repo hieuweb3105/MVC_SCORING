c108df0410a8e714114e346b230c11a7d7fd01da
Minh Hiếu <nguyenhieu3105@gmail.com>
2026-01-24 11:07:57 +0700

    update

--- public/view/btc.php
+++ public/view/btc.php
@@ -7,11 +7,11 @@
         <table class="table table-dark table-hover">
         <thead>
             <tr>
-                <th>Chương trình</th>
-                <th class="text-start">Trạng thái</th>
-                <th class="text-center">Lượt vote</th>
-                <th class="text-end">Điểm</th>
-                <th class="text-end pe-lg-3">Hành động</th>
+                <th class="bg-dark-80 blur-6">Chương trình</th>
+                <th class="text-start bg-dark-80 blur-6">Trạng thái</th>
+                <th class="text-center bg-dark-80 blur-6">Lượt vote</th>
+                <th class="text-end bg-dark-80 blur-6">Điểm</th>
+                <th class="text-end pe-lg-3 bg-dark-80 blur-6">Hành động</th>
             </tr>
         </thead>
         <tbody>
@@ -20,8 +20,8 @@
             $get_score_here = get_one_score_by_token_guest($id_show_event);
             ?>
                 <tr class="align-middle fw-light">
-                    <th class="fw-light"><?= $show['name_show_event'] ?></th>
-                    <td class="text-start">
+                    <th class="fw-light bg-dark-80 blur-6"><?= $show['name_show_event'] ?></th>
+                    <td class="text-start bg-dark-80 blur-6">
                         <div class="d-flex align-items-center justify-content-start gap-2">
                             <?php if($state_show_event == 'open') : ?>
                                 <i class="bi bi-dot text-success animate__animated animate__zoomIn animate__infinite"></i>
@@ -32,15 +32,17 @@
                             <?php endif ?>
                         </div>
                     </td>
-                    <td class="text-center"><?= count_turn_vote($id_show_event) ?></td>
-                    <td class="text-end"><?= sum_score($id_show_event) ?: 0 ?> / <?= config_get_value('config_guest') * 10 ?></td>
-                    <td class="d-flex justify-content-end flex-column flex-lg-row">
-                        <?php if(show_get_state($id_show_event) == 'open') : ?>
-                        <a href="btc/close_show/<?= $id_show_event ?>" class="btn btn-sm btn-outline-light ms-1" title="Tạm dừng chấm điểm"><i class="bi bi-pause"></i> Đóng<a>
-                        <?php else : ?>
-                        <a href="btc/open_show/<?= $id_show_event ?>" class="btn btn-sm btn-outline-light ms-1" title="Tiếp tục chấm điểm"><i class="bi bi-play"></i> Mở</a>
-                        <?php endif ?>
-                        <a href="/show/<?= $id_show_event ?>" class="btn btn-sm btn-outline-light ms-1" title="Trình chiếu điểm"><i class="bi bi-easel"></i> Chiếu</a>
+                    <td class="text-center bg-dark-80 blur-6"><?= count_turn_vote($id_show_event) ?></td>
+                    <td class="text-end bg-dark-80 blur-6"><?= sum_score($id_show_event) ?: 0 ?> / <?= config_get_value('config_guest') * 10 ?></td>
+                    <td class="text-end bg-dark-80 blur-6">
+                        <div class="d-flex justify-content-end flex-column flex-lg-row">
+                            <?php if(show_get_state($id_show_event) == 'open') : ?>
+                            <a href="btc/close_show/<?= $id_show_event ?>" class="btn btn-sm btn-outline-light w-50 ms-2 px-0" title="Tạm dừng chấm điểm"><i class="bi bi-pause"></i> Đóng<a>
+                            <?php else : ?>
+                            <a href="btc/open_show/<?= $id_show_event ?>" class="btn btn-sm btn-outline-light w-50 ms-2 px-0" title="Tiếp tục chấm điểm"><i class="bi bi-play"></i> Mở</a>
+                            <?php endif ?>
+                            <a href="/show/<?= $id_show_event ?>" class="btn btn-sm btn-outline-light w-50 ms-2 px-0" title="Trình chiếu điểm"><i class="bi bi-easel"></i> Chiếu</a>
+                        </div>
                     </td>
                 </tr>
             <?php endforeach ?>
